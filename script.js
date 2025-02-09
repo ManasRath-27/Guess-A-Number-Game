fedfc69812b371d45e8a5d1b1695205683f0e6b1
ManasRath-27 <manasranjan2709@gmail.com>
2025-02-10 01:15:11 +0530

    fully functional number guessing game

--- script.js
+++ script.js
@@ -1,20 +1,59 @@
-//game logic
-//function for generating a random number
+const messageElement = document.querySelector(".description");
+const message = messageElement.innerHTML;
+const scoreSpan = document.querySelector(".score");
+const highscoreSpan = document.querySelector(".highscore");
+
+// Generate a random number
 const min = 1;
 const max = 100;
+
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
-// console.log(getRandomNumber(min, max));
 
+const randomNumber = getRandomNumber(min, max);
+
+console.log(randomNumber);
+
+// USER INPUT
+// input is accepted when submit button is clicked
 document.getElementById("submit").addEventListener("click", function () {
-  const guessNumber = Number(document.querySelector(".guess").value);
+  let guessNumber = Number(document.querySelector(".guess").value);
 
-  //   console.log(guessNumber, typeof guessNumber);
+  // console.log(guessNumber);
 
-  if (guessNumber <= 0 || guessNumber >= 100) {
-    let message = document.querySelector(".description");
-    message.innerHTML = "Please enter a number between 1 to 100";
-    message.style.color = "#ff0000";
+  // CHECK INPUT VALIDITY
+  // We are using number input field so user can only enter numbers and nothing else so we only have to check if the input number is in valid range or not
+
+  if (guessNumber <= 0 || guessNumber > 100) {
+    messageElement.innerHTML = "Plese enter a number between 1 to 100";
+    messageElement.style.color = "#ff4500";
+  } else {
+    messageElement.innerHTML = message;
+    messageElement.style.color = "#013f2c";
   }
+
+  // IF GUESS IS RIGHT or WRONG
+  let score = Number(scoreSpan.innerHTML);
+  if (guessNumber != randomNumber && guessNumber > randomNumber) {
+    document.querySelector(".container").style.background = "#ff0000"; //#008000
+    score -= 1;
+    scoreSpan.innerHTML = score;
+    messageElement.innerHTML = "Your Guess a bit above the answer";
+  } else if (guessNumber != randomNumber && guessNumber < randomNumber) {
+    document.querySelector(".container").style.background = "#ff0000"; //#008000
+    score -= 1;
+    scoreSpan.innerHTML = score;
+    messageElement.innerHTML = "Your Guess a bit below the answer";
+  } else {
+    document.querySelector(".container").style.background = "#008000";
+    document.querySelector(".number").innerHTML = randomNumber;
+    highscoreSpan.innerHTML = score;
+    messageElement.innerHTML = "Your Guess is right 🎉";
+  }
+
+  //RESET function
+  document.getElementById("reset").addEventListener("click", function () {
+    location.reload();
+  });
 });
